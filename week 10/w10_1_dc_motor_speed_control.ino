555e1d211f8c097bce18ba9f0e18ede71899591b
Adzri9786 <84280005+Adzri9786@users.noreply.github.com>
2022-12-30 23:12:37 +0800

    Add files via upload

--- week 10/w10_1_dc_motor_speed_control.ino
+++ week 10/w10_1_dc_motor_speed_control.ino
@@ -1,20 +1,59 @@
-int motorPin = 3;  // pin to control the motor
-int speed = 0;    // variable to store the speed of the motor
+// Motor A connections
+int enA = 9; // pin to enable/disable the first motor
+int in1 = 8;
+int in2 = 7;
+// Motor B connections
+int enB = 3; // pin to enable/disable the second motor
+int in3 = 5;
+int in4 = 4;
 
 void setup() {
-  // set the motor pin as an output:
-  pinMode(motorPin, OUTPUT);
+  // Set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  
+  // Turn off motors - Initial state
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
 void loop() {
-  // increase the speed of the motor from 0 to 255:
-  for (speed = 0; speed < 256; speed++) {
-    analogWrite(motorPin, speed);
-    delay(10);
-  }
-  // decrease the speed of the motor from 255 to 0:
-  for (speed = 255; speed >= 0; speed--) {
-    analogWrite(motorPin, speed);
-    delay(10);
-  }
+  directionControl();
+  delay(1000);
+  speedControl();
+  delay(1000);
+}
+
+// This function lets you control spinning direction of motors
+void directionControl() {
+  // Set motors to maximum speed
+  // For PWM maximum possible values are 0 to 255
+  analogWrite(enA, 255);
+  analogWrite(enB, 255);
+
+  // Turn on motor A & B
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(2000);
+  
+  // Now change motor directions
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  delay(2000);
+  
+  // Now turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
